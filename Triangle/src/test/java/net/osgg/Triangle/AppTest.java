156b2d8bb2e0db17ae4ec675973b9b38e55dbd8e
Omar G. <omargomez2@gmail.com>
2021-02-08 23:50:48 -0500

    update feb21

--- Triangle/src/test/java/net/osgg/Triangle/AppTest.java
+++ Triangle/src/test/java/net/osgg/Triangle/AppTest.java
@@ -13,103 +13,41 @@ import java.io.PrintStream;
  * Unit test for simple App.
  */
 public class AppTest {
-
-  	@Test // CP para verificar clase con main
-    public void TC00() {
-    	PrintStream out = mock(PrintStream.class); //para verificar la salida a consola
-        System.setOut(out);
-        App a = new App();
-        App.main(new String[] {"1","2","3"});
-        verify(out).println(startsWith("los valores ingresados no corresponden"));
-    }
-	
 	
 	@Test
     public void TC01() {
     	TriangleUtils t = new TriangleUtils();
-    	String a = "a", b = "4", c = "4";
-    	assertTrue( t.getTriangleType(a, b, c).equals("los tres valores a ingresar deben ser números enteros") );
+    	String a = "2", b = "2", c = "2";
+    	assertTrue( t.getTriangleType(a, b, c).equals("Equilatero") );
     }
-		
-
+	
+	
 	@Test
     public void TC02() {
     	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "b", c = "4";
-    	assertTrue( t.getTriangleType(a, b, c).equals("los tres valores a ingresar deben ser números enteros") );
+    	String a = "4", b = "4", c = "2";
+    	assertTrue( t.getTriangleType(a, b, c).equals("Isósceles") );
     }
 	
 	
 	@Test
     public void TC03() {
     	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "4", c = "c";
-    	assertTrue( t.getTriangleType(a, b, c).equals("los tres valores a ingresar deben ser números enteros") );
+    	String a = "4", b = "2", c = "4";
+    	assertTrue( t.getTriangleType(a, b, c).equals("Isósceles") );
     }
+	
 	
 	
 	@Test
     public void TC04() {
     	TriangleUtils t = new TriangleUtils();
-    	String a = "1", b = "4", c = "3";
-    	assertTrue( t.getTriangleType(a, b, c).equals("los valores ingresados no corresponden con los de un triángulo") );
-    }
-
-	
-	@Test
-    public void TC05() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "3", b = "1", c = "4";
-    	assertTrue( t.getTriangleType(a, b, c).equals("los valores ingresados no corresponden con los de un triángulo") );
-    }
-	
-	
-	@Test
-    public void TC06() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "3", c = "1";
-    	assertTrue( t.getTriangleType(a, b, c).equals("los valores ingresados no corresponden con los de un triángulo") );
-    }
-	
-	
-	@Test
-    public void TC07() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "4", c = "4";
-    	assertTrue( t.getTriangleType(a, b, c).equals("Equilatero") );
-    }
-	
-	
-	@Test
-    public void TC08() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "4", c = "3";
-    	assertTrue( t.getTriangleType(a, b, c).equals("Isósceles") );
-    }
-	
-	
-	@Test
-    public void TC09() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "3", b = "4", c = "4";
-    	assertTrue( t.getTriangleType(a, b, c).equals("Isósceles") );
-    }
-	
-	
-	@Test
-    public void TC10() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "3", c = "4";
-    	assertTrue( t.getTriangleType(a, b, c).equals("Isósceles") );
-    }
-
-	
-	@Test
-    public void TC11() {
-    	TriangleUtils t = new TriangleUtils();
-    	String a = "4", b = "3", c = "2";
+    	String a = "5", b = "3", c = "4";
     	assertTrue( t.getTriangleType(a, b, c).equals("Escaleno") );
     }
+	
+	
+	
 	
 }
 
